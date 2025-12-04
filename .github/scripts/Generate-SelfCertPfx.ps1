7cd34b0a7c0a15d26cb562e8cd9e3103148e3212
HuanJin0309 <2997601264@qq.com>
2025-12-04 12:53:41 +0800

    Update Generate-SelfCertPfx.ps1
    Signed-off-by: HuanJin0309 <2997601264@qq.com>

--- .github/scripts/Generate-SelfCertPfx.ps1
+++ .github/scripts/Generate-SelfCertPfx.ps1
@@ -21,7 +21,7 @@ $cert = New-SelfSignedCertificate `
     -KeyUsage DigitalSignature `
     -FriendlyName $CertFriendlyName `
     -CertStoreLocation $CertStoreLocation `
-    -NotAfter (Get-Date).AddDays($ValidityDays)
+    -NotAfter (Get-Date).AddDays($ValidityDays) `
     -TextExtension @(
         "2.5.29.37={text}1.3.6.1.5.5.7.3.3",
         "2.5.29.17={text}DNS=$DnsName"
